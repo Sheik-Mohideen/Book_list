a4fbd93e158a586293c1eae54ef2c2b36ce7dcca
Sheik Mohideen <51860338+Sheik-Mohideen@users.noreply.github.com>
2020-08-15 16:40:25 +0530

    first commit

--- book_list/src/App.js
+++ book_list/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import BookContextProvider from './Contexts/BookContext';
+import Navbar from './components/Navbar'
+import BookList from './components/BookList';
+import BookForm from './components/BookForm';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="App"> 
+     <BookContextProvider>
+       <Navbar/>
+       <BookList/>
+       <BookForm/>
+     </BookContextProvider>
     </div>
   );
 }

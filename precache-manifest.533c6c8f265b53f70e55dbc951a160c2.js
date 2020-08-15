310c010a71db8e32d45175ac6ca8348e99900d7e
Sheik Mohideen <51860338+Sheik-Mohideen@users.noreply.github.com>
2020-08-15 16:54:49 +0530

    Updates

---
+++ precache-manifest.533c6c8f265b53f70e55dbc951a160c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8dac0f30bf5fc2868fc6bbaf7a786ee",
+    "url": "/Book_list/index.html"
+  },
+  {
+    "revision": "9f86d94c6278b8bd508e",
+    "url": "/Book_list/static/css/main.e5dcd167.chunk.css"
+  },
+  {
+    "revision": "5790fc08a410bb9c93ed",
+    "url": "/Book_list/static/js/2.ab40918c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Book_list/static/js/2.ab40918c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f86d94c6278b8bd508e",
+    "url": "/Book_list/static/js/main.ab6c03fc.chunk.js"
+  },
+  {
+    "revision": "7f4b07485262e33b4535",
+    "url": "/Book_list/static/js/runtime-main.c13fd13e.js"
+  }
+]);
